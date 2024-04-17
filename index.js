b356a51014e63139d915d45a4cd9140c31da991f
Akanksha S <Akanksha.Lakhera@gmail.com>
2024-04-17 17:56:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,121 +1,74 @@
-'use strict';
-
-const express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
-var mongoose = require('mongoose');
-var shortId = require('shortid');
-var bodyParser = require('body-parser');
-var validUrl = require('valid-url');
-const dns = require('dns');
 require('dotenv').config();
+const express = require('express');
+const validUrl = require('valid-url');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const bodyParser = require('body-parser');
+const validator = require("validator");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
+app.use(express.json()); // Parse JSON in the request body
+app.use(bodyParser.urlencoded({extented: true}));
 app.use(cors());
-app.use(express.json());
-
-const uri = process.env.MONGO_URI;
-//console.log(uri);
-
-mongoose.connect(uri, {
-  serverSelectionTimeoutMS: 5000
-});
-
-const connection = mongoose.connection;
-
-connection.once('open', () => {
-  console.log("MongoDB connection established successfully");
-})
 
 app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function (req, res) {
+let urlMapping = {};
+let counter = 0;
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-//Create Schema
+function validateUrl(req, res, next) {
+  // const url = req.body && req.body.url;
+  const { url } = req.body;
 
-const Schema = mongoose.Schema;
-const urlSchema = new Schema({
-  original_url: String,
-  short_url: String
-})
+  validate(url,req, res, next);
+}
 
-const URL = mongoose.model("URL", urlSchema);
+function validate(url, req, res, next) {
+  if (!url || !validUrl.isWebUri(url)) {
+    return res.json({ error: 'invalid url' });
+  }
 
+  // Create a new variable to store the validated URL
+  const validatedUrl = new URL(url);
+
+  const httpRegex = /^(https)(:\/\/)/;
+  if (!httpRegex.test(url)) {return res.json({ error: 'invalid url' })}
+
+
+    dns.lookup(validatedUrl.hostname, (err, addresses) => {
+     if(!addresses) {
+        return res.send({ error: 'invalid url' });
+      }
+    next();
+  });
+} 
 // Your first API endpoint
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl',
+ validateUrl,
+ function(req, res) {
+  const url = req.body.url;
+  const shortUrl = ++counter; // Increment counter for each new URL  
+  urlMapping[counter] = url;  
+  res.json({ original_url: url, short_url: shortUrl });
 });
 
-app.post('/api/shorturl/', async function (req, res) {
-  var url = req.body.url;
-  const urlCode = shortId.generate();
-
-  console.log(url);
-
-  //Check for valid url
-  // if (!validUrl.isWebUri(url)) {
-  //   res.status(401).json({
-  //     error: "invalid URL"
-  //   })
-  if(dns.lookup(url, (err, addresses) => console.log(err))){
-    res.status(401).json({
-      error: "Invalid URL"
-    })
-  } else {
-    try {
-      //check DB for existing url
-      let findOne = await URL.findOne({
-        original_url: url
-      })
-      if (findOne) {
-        res.json({
-          original_url: findOne.original_url,
-          short_url: findOne.short_url
-        })
-      } else {
-        //if not found the create new and return
-        findOne = new URL({
-          original_url: url,
-          short_url: urlCode
-        })
-        await findOne.save()
-        res.json({
-          original_url: findOne.original_url,
-          short_url: findOne.short_url
-        })
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json('Server error....')
-    }
-  }
-})
-
-app.get('/api/shorturl/:short_url?', async function (req, res) {
-  try{
-    const urlParams = await URL.findOne({
-      short_url: req.params.short_url
-    })
-    if (urlParams) {
-      return res.redirect(urlParams.original_url);
+app.get('/api/shorturl/:shorturl',
+  function(req, res) {
+    const id = parseInt(req.params.shorturl);
+    const url = urlMapping[id]
+    if (url) {
+      validate(url,req, res, ()=> {
+         res.redirect(urlMapping[id])
+      });
     } else {
-      return res.status(404).json('Server error');
+      res.json({error: 'Short URL not found'})
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json('Server error')
-  }
-})
-
-app.listen(port, function () {
+ });
+ 
+app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
